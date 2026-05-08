4288db97aa75bd6bc5a7d2300f3f9a185fe293af
Radu Blendea <hsky8689@gmail.com>
2026-05-08 13:06:10 +0300

    Refactor DBServicePersoane for cleaner formatting

--- src/main/java/service/DBServicePersoane.java
+++ src/main/java/service/DBServicePersoane.java
@@ -11,8 +11,7 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-public class DBServicePersoane extends DBService<Persoana>
-        implements Observable<ObserverGUI, CRUDActions> {
+public class DBServicePersoane extends DBService<Persoana> implements Observable<ObserverGUI, CRUDActions> {
 
     private final ObservableSupport obs = new ObservableSupport();
 
@@ -21,11 +20,11 @@ public class DBServicePersoane extends DBService<Persoana>
     }
 
     @Override
-    public void addObserver(ObserverGUI o)                            { obs.addObserver(o); }
+    public void addObserver(ObserverGUI o) { obs.addObserver(o); }
     @Override
-    public void removeObserver(ObserverGUI o)                         { obs.removeObserver(o); }
+    public void removeObserver(ObserverGUI o) { obs.removeObserver(o); }
     @Override
-    public void notifyObservers(CRUDActions action, Object payload)   { obs.notifyObservers(action, payload); }
+    public void notifyObservers(CRUDActions action, Object payload) { obs.notifyObservers(action, payload); }
 
     @Override
     public boolean adauga(Persoana o) {
